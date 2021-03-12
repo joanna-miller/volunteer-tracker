e7884e98bbc68856e3be5258f96cebb3c1bf31bc
Jo <joannadawnmiller@gmail.com>
2021-03-12 13:27:53 -0500

    pass third integration spec to update project title

--- app.rb
+++ app.rb
@@ -28,3 +28,14 @@ get('/projects/:id') do
   @project = Project.find(params[:id].to_i)
   erb(:project)
 end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i)
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i)
+  @project.update({title: params[:title]})
+  redirect to('/projects')
+end
