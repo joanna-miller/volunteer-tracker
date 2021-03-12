4daf768c222b533b3ecd02866ccddf9a3d3b7646
Jo <joannadawnmiller@gmail.com>
2021-03-12 12:46:48 -0500

    add form to create project, pass first integration test

---
+++ app.rb
@@ -0,0 +1,25 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:title]
+  project = Project.new({title: title})
+  project.save
+  redirect to('/projects')
+end
